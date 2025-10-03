aa9286f6e27a52d67ba5728e827be6db826e8996
DianaZhou-accd <jzhou10@inside.artcenter.edu>
2025-10-03 11:09:03 -0700

    Update script.js

--- week 3/inclass/scripts/script.js
+++ week 3/inclass/scripts/script.js
@@ -1,9 +1,13 @@
 let cBox = document.getElementById("colorBox");
 let colorBtn = document.getElementById("changeColor");
 
+let assignRandomColor = function()
+{
+    cBox.style.backgroundColor ="rgb(255,0,0)";
+}
+
 console.log(cBox);
 
-cBox.style.backgroundColor ="rgb(255,0,0"
 
 
 colorBtn.addEventListener("click", assignRandomColor);
