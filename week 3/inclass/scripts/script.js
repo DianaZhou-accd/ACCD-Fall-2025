285c0faf47207d14a48c67d0ebe94f1548e25e15
DianaZhou-accd <jzhou10@inside.artcenter.edu>
2025-10-03 11:04:57 -0700

    Create script.js

---
+++ week 3/inclass/scripts/script.js
@@ -0,0 +1,9 @@
+let cBox = document.getElementById("colorBox");
+let colorBtn = document.getElementById("changeColor");
+
+console.log(cBox);
+
+cBox.style.backgroundColor ="rgb(255,0,0"
+
+
+colorBtn.addEventListener("click")
