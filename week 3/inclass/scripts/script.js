e6d81a11c3810577fcc4c8937fb9dd933e9b5299
DianaZhou-accd <jzhou10@inside.artcenter.edu>
2025-10-03 12:20:47 -0700

    Update script.js

--- week 3/inclass/scripts/script.js
+++ week 3/inclass/scripts/script.js
@@ -1,5 +1,7 @@
 let cBox = document.getElementById("colorBox");
 let colorBtn = document.getElementById("changeColor");
+let imgBox = document.getElementById("crocodileImage");
+let imageBtn = document.getElementById("toggleImage");
 
 let assignRandomColor = function()
 {
@@ -9,6 +11,19 @@ let assignRandomColor = function()
     cBox.style.backgroundColor ="rgb(" +rComp +", " + gComp +", " + bComp +")";
 }
 
-console.log(cBox);
+const toggleCrocodileImage = () =>
+{
+    console.log(imgBox.src);  
+    if (imgBox.src.includes("baby-crocodile"))
+    {
+        console.log("Changing to adult crocodile");
+        imgBox.src = "pictures/crocodile-2.jpg";
+    }
+    else
+    {
+        imgBox.src ="pictures/baby-crocodile.webp";
+    }
+}
 
+imageBtn.addEventListener("click", toggleCrocodileImage);
 colorBtn.addEventListener("click", assignRandomColor);
