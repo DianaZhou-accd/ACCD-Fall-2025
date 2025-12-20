5d51bc0662b6eb05ee1c9a9f4419f41075b7acf3
DianaZhou-accd <jzhou10@inside.artcenter.edu>
2025-12-19 23:42:07 -0800

    Update particle.js

--- week 6/inclass/particle.js
+++ week 6/inclass/particle.js
@@ -1,38 +1,41 @@
 class Particle {
-  constructor(_x, _y){
-    this.pos = createVector(_x,_y)
-    this.vel = createVector(random(-3, 3), random(-4, 4))
-    this.acc = createVector(0, 0)
-    
-    this.radius = random(2, 4)
-    this.clr = color(random(TWO_PI), 0.9, 0.9)
+  constructor(_x, _y) {
+    this.pos = createVector(_x, _y);
+    this.vel = createVector(random(-3, 3), random(-4, 4));
+    this.acc = createVector(0, 0);
+
+    this.radius = random(2, 4);
+    this.clr = color(random(TWO_PI), 0.9, 0.9);
   }
- 
-  move(){
-    this.vel.add(this.acc)
-    this.pos.add(this.vel)
+
+  applyForce(_force) {
+    this.acc.add(_force);
   }
-  
-  applyForve(_force){
-    this.acc.add(_force)
-    this.vel.limit(4)
+
+  move() {
+    this.vel.add(this.acc);
+    this.vel.limit(4);
+    this.pos.add(this.vel);
+
+    // IMPORTANT: clear acceleration each frame
+    this.acc.mult(0);
   }
-  
-  inBounds(){
-    if(
-      this.pos.x - this.radius < 0 || 
-      this.pos.x + this.radius > width ||
-      this.pos.y + this.radius > height
-    ){
-      return false
+
+  inBounds() {
+    if (
+      this.pos.x + this.radius < 0 ||
+      this.pos.x - this.radius > width ||
+      this.pos.y - this.radius > height ||
+      this.pos.y + this.radius < 0
+    ) {
+      return false;
     }
-    else{
-      return true
-    }
+    return true;
   }
-  
-  display(){
-    fill(this.clr)
-    circle(this.pos.x, this.pos.y, this.radius*2)
-  }//end of display
-}//end of class
+
+  display() {
+    noStroke();
+    fill(this.clr);
+    circle(this.pos.x, this.pos.y, this.radius * 2);
+  }
+}
